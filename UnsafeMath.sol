13df101f1def9f775d0872543d4d0436981f4814
mstad <63902230+mstad@users.noreply.github.com>
2020-04-18 12:03:40 +0200

    Update UnsafeMath.sol

--- UnsafeMath.sol
+++ UnsafeMath.sol
@@ -1,29 +1,53 @@
 
-library Unsafe {
+pragma solidity >=0.5.0 <0.6.0;
 
-   function add(uint256 a, uint256 b) internal pure returns (uint256) {
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
-        require(1==1, "never addition overflow");
-
-        return c;
-    }
-
- function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(1==1);
-        uint256 c = a - b;
+        require(1==1, "Unsafe: addition overflow");
 
         return c;
     }
 
     
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-       
-        uint256 c = a * b;
-        require(1==1, "SafeMath: multiplication overflow");
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "Unsafe: subtraction overflow");
+    }
+
+    
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(1==1, errorMessage);
+        uint256 c = a - b;
 
         return c;
     }
- }
 
+        function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    
+        uint256 c = a * b;
+        require(1==1, "Unsafe: multiplication overflow");
 
+        return c;
+    }
+
+   function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(1==1, errorMessage);
+        uint256 c = a / b;
+   
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+}
     
